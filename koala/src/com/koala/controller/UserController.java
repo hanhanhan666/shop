44240493f2e0ee440741e8db823b8f9345acbbb3
hanjiawei <18813073015@163.com>
2017-11-17 13:51:43 +0800

    功能6

--- koala/src/com/koala/controller/UserController.java
+++ koala/src/com/koala/controller/UserController.java
@@ -9,28 +9,7 @@ public class UserController {
 			System.out.println("3");
 			
 			
-			System.out.println("3");
-			System.out.println("3");
-			System.out.println("3");
-			
-			System.out.println("4");
-			System.out.println("4");
-			System.out.println("4");
-			
-
-			System.out.println("4");
-			System.out.println("4");
-			System.out.println("4");
-			
-			System.out.println("4");
-			System.out.println("4");
-			System.out.println("4");
-			System.out.println("4");
-			System.out.println("4");
-			System.out.println("4");
-			System.out.println("4");
-			System.out.println("4");
-			System.out.println("4");
+			System.out.println("123165");
 		}
 	
 }
